a3d02d5afb79be8b974a21a4a652d642efa3ea68
braunjon <25931090+braunjon@users.noreply.github.com>
2021-11-25 21:21:48 +0100

    loop through all log dirs

--- plot_results.py
+++ plot_results.py
@@ -55,20 +55,50 @@ def plot_tensorflow_log(log_file_path_list, legend_names, fname,
     plt.show()
 
 
+def gather_all_log_paths(main_log_path, env_dirs):
+    log_paths = list()
+    for env_dir in env_dirs:
+        env_log_path = os.path.join(main_log_path, env_dir)
+
+        exp_dirs = [directory for directory in os.listdir(env_log_path) if os.path.isdir(os.path.join(env_log_path, directory))]
+
+        for exp_dir in exp_dirs:
+            log_paths.append(os.path.join(env_log_path, exp_dir))
+
+    return log_paths
+
+
 if __name__ == '__main__':
-    log_path = "./logs/Tensorboard_MAML45/discount-f"
-    out_file_name = log_path.replace("/", "_")[2:]
-    dirs = [directory for directory in os.listdir(log_path) if os.path.isdir(os.path.join(log_path, directory))]
-    dirs.sort()
 
-    log_file_path_list = list()
-    folder_names = list()
-    for log_dir in dirs:
-        log_dir_path = os.path.join(log_path, log_dir)
-        # filter for tensor board logs
-        log_file = [file for file in os.listdir(log_dir_path) if "events" in file][0]
-        log_file_path = os.path.join(log_dir_path, log_file)
-        folder_names.append(log_dir)
-        log_file_path_list.append(log_file_path)
+    main_log_path = "./logs/"
+    env_dirs = ['Tensorbaord_PEARL10',
+                'Tensorboard_MAML10',
+                'Tensorboard_MAML1_Basketball',
+                'Tensorboard_MAML45',
+                'Tensorboard_PEARL1_Basketball']
 
-    plot_tensorflow_log(log_file_path_list, legend_names=folder_names, fname=out_file_name)
+    log_paths = gather_all_log_paths(main_log_path=main_log_path, env_dirs=env_dirs)
+
+    # Overwrite log paths with specific experiment to only plot specific experiments
+    # log_paths = ["./logs/Tensorboard_MAML45/discount-f"]
+
+    for log_path in log_paths:
+        print("---------------")
+        print(log_path)
+        print("---------------")
+
+        out_file_name = log_path.replace("/", "_")[2:]
+        dirs = [directory for directory in os.listdir(log_path) if os.path.isdir(os.path.join(log_path, directory))]
+        dirs.sort()
+
+        log_file_path_list = list()
+        folder_names = list()
+        for log_dir in dirs:
+            log_dir_path = os.path.join(log_path, log_dir)
+            # filter for tensor board logs
+            log_file = [file for file in os.listdir(log_dir_path) if "events" in file][0]
+            log_file_path = os.path.join(log_dir_path, log_file)
+            folder_names.append(log_dir)
+            log_file_path_list.append(log_file_path)
+
+        plot_tensorflow_log(log_file_path_list, legend_names=folder_names, fname=out_file_name)
